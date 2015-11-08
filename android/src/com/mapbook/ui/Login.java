dd288e8d98b0dc7d96f4c82ea54681c072216386
Rico Tiongson <thericotiongson@gmail.com>
2015-11-09 00:39:40 +0800

    Edited register UI.

--- android/src/com/mapbook/ui/Login.java
+++ android/src/com/mapbook/ui/Login.java
@@ -9,6 +9,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
