268d3a83b1c200ccc18294b8e1050fac9f6f02f8
Rico Tiongson <thericotiongson@gmail.com>
2015-11-24 21:18:17 +0800

    Created "Add sample marker" button.

--- android/src/com/mapbook/ui/MarkerView.java
+++ android/src/com/mapbook/ui/MarkerView.java
@@ -3,8 +3,11 @@ package com.mapbook.ui;
 import com.mapbook.locationsaver.R;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -24,10 +27,30 @@ public class MarkerView extends MapbookActivity {
 	
 	// components
 	private TextView header;
+	private LinearLayout markers;
 	
 	// prepare component fonts and variables
-	void prepareComponents() {
+	private void prepareComponents() {
+		
 		header = (TextView) findViewById(R.id.marker_view_header);
+		markers = (LinearLayout) findViewById(R.id.marker_view);
+		
+		Button button = (Button) findViewById(R.id.buttonz);
+		button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				// TODO Auto-generated method stub
+				MarkerView.this.addEntry();
+			}
+		});
+		button.setText("Add sample marker");
+	}
+	
+	// adds an empty entry to the marker view list
+	public LinearLayout addEntry() {
+		LinearLayout entry = (LinearLayout) getLayoutInflater().inflate(R.layout.marker_view_entry, null);
+		markers.addView(entry); // empty entry
+		return entry;
 	}
 	
 }
